837ad247ef2649d5acde3d6db3ab8b4909aa17fa
Tentabros <reuel.lichangco@gmail.com>
2026-02-11 13:41:50 +0800

    Add files via upload

---
+++ exercise-2/index.js
@@ -0,0 +1,41 @@
+function startTime(){
+
+    const today = new Date();
+    let hour = today.getHours();
+    let mins = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    let am_pm = hour >= 12 ? 'PM' : 'AM';
+
+    let messagebox='';
+    
+    hour = hour%12;
+
+    if(!hour){
+        hour = 12;
+    }
+    mins =checkTime(mins);
+    seconds = checkTime(seconds);
+
+    if(am_pm == 'AM' && hour > 0){
+        messagebox = "Good Morning!";
+    }else{
+        if(hour < 6){
+            messagebox = "Good Afternoon!";
+        }else{
+            messagebox = "Good Evening";
+        }
+    }
+
+    document.getElementById("messageBox").innerHTML = messagebox;
+
+    document.getElementById("time-now").innerHTML = hour+":"+mins+":"+seconds+':'+am_pm;
+
+    setTimeout(startTime, 1000);
+
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  
+    return i;
+}
